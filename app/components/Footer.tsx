3b7a6cdd9409a0c0ea2c307951844b7e18221c45
dev-verde1071 <deyessoluke689@gmail.com>
2025-09-27 15:31:24 -0400

    Create Footer.tsx

---
+++ app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="col-span-12 text-center py-6 text-sm text-[#6B5B4D] bg-[#EFE7DD] rounded-2xl">
+      <div>© {new Date().getFullYear()} Access Rack Solutions Inc.</div>
+    </footer>
+  );
+}
