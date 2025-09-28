e372aa4285be026fa226734bcfd1759ab57a43be
dev-verde1071 <deyessoluke689@gmail.com>
2025-09-28 12:10:52 -0400

    Create page.js

---
+++ app/portfolio/page.js
@@ -0,0 +1,8 @@
+export default function PortfolioPage() {
+  return (
+    <main style={{ marginTop: "2rem" }}>
+      <h1>Portfolio</h1>
+      <p>Check out our completed upfitting projects and satisfied customer installations.</p>
+    </main>
+  );
+}
