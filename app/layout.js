eabaeb8b52eb05d7db0a4755086d96511bbd4dd7
dev-verde1071 <deyessoluke689@gmail.com>
2025-09-28 18:22:59 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,25 +1,14 @@
-import "./globals.css";
-import { CartProvider } from "../components/CartContext";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+"use client";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;600;700&display=swap"
-        />
-      </head>
       <body>
-        <CartProvider>
-          <Header />
-          <div style={{ maxWidth: "1200px", margin: "0 auto", padding: "1rem" }}>
-            {children}
-          </div>
-          <Footer />
-        </CartProvider>
+        <Elements stripe={stripePromise}>{children}</Elements>
       </body>
     </html>
   );
