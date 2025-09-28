0af0c5fade91ab56d5c98b465540164de7545ab3
dev-verde1071 <deyessoluke689@gmail.com>
2025-09-28 12:10:14 -0400

    Create page.js

---
+++ app/company/page.js
@@ -0,0 +1,8 @@
+export default function CompanyPage() {
+  return (
+    <main style={{ marginTop: "2rem" }}>
+      <h1>About Our Company</h1>
+      <p>Access Rack Solutions Inc. provides high-quality commercial van upfitting solutions in Orlando, FL.</p>
+    </main>
+  );
+}
