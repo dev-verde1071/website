1a2f390dba894298e7ac99e981af18db4231a65c
dev-verde1071 <deyessoluke689@gmail.com>
2025-09-28 10:35:52 -0400

    Create page.js

---
+++ app/success/page.js
@@ -0,0 +1,10 @@
+export default function SuccessPage() {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-4xl font-bold text-green-700 mb-4">
+        ✅ Payment Successful!
+      </h1>
+      <p className="text-lg">Thank you for your order.</p>
+    </div>
+  );
+}
