c6506f3342b9e63fb4e35a6d59022c3d1a3544a6
dev-verde1071 <deyessoluke689@gmail.com>
2025-09-28 17:55:57 -0400

    Create page.js

---
+++ app/success/page.js
@@ -0,0 +1,9 @@
+export default function SuccessPage() {
+  return (
+    <main style={{ textAlign: "center", marginTop: "2rem" }}>
+      <h1>🎉 Payment Successful!</h1>
+      <p>Thank you for your order. We’ll be in touch soon.</p>
+      <a href="/">Back to Home</a>
+    </main>
+  );
+}
