f121e836f4b93746eeb9516b59a2a08c575cf7fa
dev-verde1071 <deyessoluke689@gmail.com>
2025-09-28 12:11:10 -0400

    Create page.js

---
+++ app/shop/page.js
@@ -0,0 +1,26 @@
+"use client";
+import { useContext } from "react";
+import { CartContext } from "../../components/CartContext";
+import ProductCard from "../../components/ProductCard";
+
+const products = [
+  { id: 1, name: "Van Shelving", price: 499 },
+  { id: 2, name: "Ladder Rack", price: 299 },
+  { id: 3, name: "Partition", price: 399 },
+  { id: 4, name: "Accessory Kit", price: 149 }
+];
+
+export default function ShopPage() {
+  const { addToCart } = useContext(CartContext);
+
+  return (
+    <main style={{ marginTop: "2rem" }}>
+      <h1>Shop Products</h1>
+      <div style={{ display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))", gap: "1rem" }}>
+        {products.map((p) => (
+          <ProductCard key={p.id} product={p} addToCart={addToCart} />
+        ))}
+      </div>
+    </main>
+  );
+}
