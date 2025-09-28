9c1cfc76818ad5fd1ae5e294aa7c2b6471ab98c7
dev-verde1071 <deyessoluke689@gmail.com>
2025-09-28 12:10:33 -0400

    Create page.js

---
+++ app/mission/page.js
@@ -0,0 +1,8 @@
+export default function MissionPage() {
+  return (
+    <main style={{ marginTop: "2rem" }}>
+      <h1>Our Mission</h1>
+      <p>We aim to provide affordable, reliable, and durable upfitting solutions for commercial vans nationwide.</p>
+    </main>
+  );
+}
